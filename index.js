4c208e769bac33a34c145512e50b1570ac41c34b
Milind Chaudhari <milind220397@gmail.com>
2022-11-19 18:31:24 +0530

    PAYMENT INTEGRATION

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const cors = require('cors')
-//const routes = require('./routes/routes')
-//require("dotenv").config();
 const dbConnect = require('./mysql');
 const app = express();
+const shortid = require('shortid')
+const Razorpay = require('razorpay')
 const PORT = process.env.PORT;
 app.use(cors())
 app.use(express.json())
 
-    
+
 const profile = require('./routes/profile');
 const productdet = require('./routes/productdet')
-app.use('/profile',profile);  
-app.use('/productdet',productdet);      
+const payment = require('./routes/payment');
+app.use('/profile', profile);
+app.use('/productdet', productdet);
+app.use('/razorpay', payment);
 
-app.listen(3000, () => {
+app.listen(1337, () => {
   console.log(`server running at 127.0.0.1:${PORT}`);
 });
