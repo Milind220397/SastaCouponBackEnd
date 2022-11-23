1fd3a14b6b6d0987dfd7ef803c79ff4e07c50140
Haja Asmath <asmath1997@gmail.com>
2022-11-24 02:15:40 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require('cors')
-const dbConnect = require('./mysql');
 const app = express();
 const shortid = require('shortid')
 const Razorpay = require('razorpay')
 const PORT = process.env.PORT;
 const logger = require('./src/utils/logger');
 const AuthRoutes = require('./src/routes/AuthRoutes') 
-const uploadCouponRoutes = require('./src/routes/UploadCouponRoutes');
-app.use(require('cookie-parser')());
+const couponRoutes = require('./src/routes/CouponRoutes');
+app.use(require('cookie-parser')(process.env.COOKIE_SECRET));
 require('./mysql');
 app.use(cors())
 app.use(express.json())
@@ -18,12 +17,13 @@ app.use(express.json())
 const profile = require('./src/routes/profile');
 const productdet = require('./src/routes/productdet')
 const payment = require('./routes/payment');
+const cookieParser = require('cookie-parser');
 app.use('/profile', profile);
 app.use('/productdet', productdet);
 app.use('/razorpay', payment);
 app.use('/', AuthRoutes);
-app.use('/', uploadCouponRoutes);
+app.use('/', couponRoutes);
 
 app.listen(PORT, () => {
   logger.info(`server running at 127.0.0.1:${PORT}`);
-});
+}); 
--- src/repos/CouponRepo.js
+++ src/repos/CouponRepo.js
@@ -1,8 +1,12 @@
 const db = require('../../mysql');
 
 const insertCoupon = async (coupon) => {
-    return await db.promise().query(`INSERT INTO sastacoupon.COUPON (NAME, DESCRIPTION, EXPIRY, PRICE, SELLER_ID, IMAGE_ID, CREATE_TIMESTAMP, COUPON_CODE) VALUES (
-        ${coupon.couponName}, ${coupon.descrption}, ${coupon.expiryDate}, ${coupon.sellerId}, ${coupon.imageId}, ${new Date()}, ${coupon.couponCode}`);
+    const expiryDate = new Date(coupon.expiryDate).toISOString().slice(0,10);
+    return await db.promise().query(`INSERT INTO sastacoupon.COUPON (NAME, DESCRIPTION, EXPIRY, PRICE, SELLER_ID, IMAGE_ID, COUPON_CODE) VALUES ('${coupon.couponName}', '${coupon.couponDiscription}', '${expiryDate}',${coupon.denomination}, ${coupon.userId}, ${coupon.imageId}, '${coupon.couponCode}')`);
 }
 
-module.exports = {insertCoupon}
+const findByRecent = () => {
+    return db.promise().query(`SELECT c.ID, NAME, PRICE, URL FROM COUPON c left join COUPON_IMAGE ci on c.IMAGE_ID = ci.ID ORDER BY CREATED_TIMESTAMP LIMIT 8`)
+}
+
+module.exports = {insertCoupon, findByRecent}
