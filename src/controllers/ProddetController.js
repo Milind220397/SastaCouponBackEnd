a40c8810d8791616bff2fb3cd9b3535779bb317e
Milind Chaudhari <milind220397@gmail.com>
2022-11-26 07:18:54 +0530

    Changes verifivation

--- src/controllers/ProddetController.js
+++ src/controllers/ProddetController.js
@@ -12,12 +12,12 @@ const razorpay = new Razorpay({
 
 const payment = async (req, res) => {
     console.log("Inside razor post");
-    // console.log(req);
+    console.log(req.body);
     const payment_capture = 1
     const amount = req.body.amount * 100;
     console.log(amount);
     const currency = 'INR';
-
+    var sql = 'INSERT INTO ORDER_DETAILS (ORDER_ID,COUPON_ID,STATUS,BUYER_ID,TRANSACTION_TYPE,PAYMENT_TIMESTAMP,PAYMENT_ID)VALUES ?';
     const options = {
         amount: amount,
         currency,
@@ -27,10 +27,34 @@ const payment = async (req, res) => {
     try {
         const response = await razorpay.orders.create(options)
         console.log(response)
+        // Updateorder id in db 
+
+        var values = [
+            [response.id, req.body.coupon_id, response.status, req.body.id, 'Bought', response.created_at,""]
+        ];
+      
+            db.query(sql, [values], function (err, result, fields) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log('Executed Successfully');
+                    console.log(result.affectedRows + " record(s) updated");
+                }
+                if (result.length === 0) {
+                    res.send("Incorrect Id")
+                }
+                else {
+                    console.log("" + JSON.stringify(result));
+                    // res.send(JSON.stringify(result.affectedRows));
+                }
+            });
+        //**************************
         res.json({
             id: response.id,
             currency: response.currency,
-            amount: response.amount
+            amount: response.amount,
+            order_id:response.id
         })
     } catch (error) {
         console.log(error)
@@ -42,38 +66,26 @@ const verification = (req, res) => {
     console.log("inside verifuy");
     let user_id = req.query.id;
     var result1;
-    var sql = 'INSERT INTO ORDER_DETAILS (ORDER_ID,COUPON_ID,STATUS,BUYER_ID,TRANSACTION_TYPE,PAYMENT_TIMESTAMP,PAYMENT_ID)VALUES ?';
-
-    // const db = mysql.createConnection({
-    //     host: 'sastacoupon.c5lcdzbaqcbr.ap-northeast-1.rds.amazonaws.com',
-    //     user: 'admin',
-    //     password: 'sastacoupon123',
-    //     database: 'sastacoupon'
-    // })
+    var sql = 'UPDATE  ORDER_DETAILS SET  STATUS = ? ,PAYMENT_TIMESTAMP = ? ,PAYMENT_ID = ? WHERE ORDER_ID = ?';
+    
 
     const secret = 'sastacoupon123'
     console.log(req.body)
     const res_verify = req.body.payload.payment.entity;
     console.log(req.body.payload.payment.entity)
+
     // *****Verifing the Signature *******************
     const crypto = require('crypto')
     const shasum = crypto.createHmac('sha256', secret)
     shasum.update(JSON.stringify(req.body))
     const digest = shasum.digest('hex')
     console.log(digest, req.headers['x-razorpay-signature'])
+
+
+    // Verifying the Signature 
     if (digest === req.headers['x-razorpay-signature']) {
-        console.log('request is legit')
-        // *********************** Send payment detils to database***************************
-        var values = [
-            [res_verify.order_id, '1234', res_verify.status, user_id, 'Bought', res_verify.created_at,res_verify.id]
-        ];
-        // db.connect((err) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log("MYSQL CONNECTED")
-        //     }
-            db.query(sql, [values], function (err, result, fields) {
+        console.log('Request is legit')
+            db.query(sql, [res_verify.status,res_verify.created_at,res_verify.id,res_verify.order_id], function (err, result, fields) {
                 if (err) {
                     console.log(err);
                 }
@@ -104,23 +116,10 @@ const product_details = (req, res) => {
     logger.info(`Product Detail Controller Triggered`);
     let coupon_id =req.query.id;
     console.log(req.query);
-   
+
     var result1;
     var sql = 'SELECT a.ID,a.NAME, a.DESCRIPTION,a.EXPIRY,a.PRICE,a.SELLER_ID,a.BUYER_ID,a.IMAGE_ID,a.CREATED_TIMESTAMP,a.COUPON_CODE, b.URL, b.OCCASION ,b.DAFAULT_IMAGE  FROM COUPON AS a INNER JOIN COUPON_IMAGE AS b ON a.IMAGE_ID=b.ID WHERE a.ID = ?';
 
-    const db = mysql.createConnection({
-        host: 'sastacoupon.c5lcdzbaqcbr.ap-northeast-1.rds.amazonaws.com',
-        user: 'admin',
-        password: 'sastacoupon123',
-        database: 'sastacoupon'
-    })
-
-    db.connect((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("MYSQL CONNECTED")
-        }
 
         db.query(sql, [coupon_id], function (err, result, fields) {
             if (err) {
@@ -136,7 +135,7 @@ const product_details = (req, res) => {
                 res.send(JSON.stringify(result[0]));
             }
         });
-    })
+    //})
 }
 
 module.exports = {  payment, verification, product_details }
